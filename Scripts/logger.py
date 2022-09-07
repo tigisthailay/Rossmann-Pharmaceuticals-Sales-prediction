65d5bdbd5a9350e6e5f8f89b65e6526859aa5670
Tegisty Hailay <tigisthay13@gmail.com>
2022-09-07 12:11:59 +0300

    dvc, mlflow

--- Scripts/logger.py
+++ Scripts/logger.py
@@ -1,32 +1,23 @@
-"""Log messages to a file."""
-
 import logging
 
 
-class Logger:
-    """Logger class for logging messages to a file."""
+class App_Logger:
 
-    def __init__(self, file_name: str, basic_level=logging.INFO):
-        """Initilize logger class with file name to be written and default log level.
+  def __init__(self, file_name: str, basic_level=logging.INFO):
+    # Gets or creates a logger
+    logger = logging.getLogger(__name__)  
 
-        """
-        # createing a logger
-        logger = logging.getLogger(__name__)
+    # set log level
+    logger.setLevel(basic_level)
 
-        # set log level
-        logger.setLevel(basic_level)
+    # define file handler and set formatter
+    file_handler = logging.FileHandler(file_name)
+    formatter    = logging.Formatter('%(asctime)s : %(levelname)s : %(name)s : %(message)s')
+    
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
 
-        # define file handler and set formatter
-
-        file_handler = logging.FileHandler(f'../logs/{file_name}')
-        formatter = logging.Formatter(
-            '%(asctime)s : %(levelname)s : %(name)s : %(message)s', '%m-%d-%Y %H:%M:%S')
-
-        file_handler.setFormatter(formatter)
-        logger.addHandler(file_handler)
-
-        self.logger = logger
-
-    def get_app_logger(self) -> logging.Logger:
-        """Return the logger object. """
-        return self.logger
+    self.logger = logger
+  
+  def  get_app_logger(self) -> logging.Logger:
+    return self.logger
